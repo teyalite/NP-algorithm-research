0da5c4fad87d27841f1c7c4188eb5cd11732c458
= <66052642+teyaligth@users.noreply.github.com>
2022-04-11 17:46:08 +0300

    Updated main.py

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ import matplotlib.pyplot as plt
 from solutions import BruteForce, DynamicProgramming, Approximation
 from test import favorite_test_case
 
-brute_force = BruteForce(favorite_test_case[0], favorite_test_case[1])
-dynamic_programming = DynamicProgramming(favorite_test_case[0], favorite_test_case[1])
+brute_force = BruteForce(favorite_test_case[0], favorite_test_case[1].copy())
 cost, path = brute_force.solution()
 print(cost, path)
-print(dynamic_programming.solution())
 brute_force.draw(path)
 plt.show()
